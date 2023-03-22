9db00f7a4fd798f759668162520849c41fa67732
bekzod818 <bekzod030900@gmail.com>
2023-03-22 23:44:17 +0500

    fix: update

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()  # read .env file, if it exists
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -133,3 +140,9 @@ PAYME: dict = {
     'PAYME_MIN_AMOUNT': 'payme-min-amount', # integer field
     'PAYME_ACCOUNT': 'order-id',  # payme kassa rekviziti
 }
+
+# KASSA_ID
+KASSA_ID = env.str("KASSA_ID") 
+
+# SECRET_KEY
+SECRET_KEY = env.str("SECRET_KEY") 
--- payme/tests.py
+++ payme/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from payme.cards.subscribe_cards import PaymeSubscribeCards
+from django.conf import settings
 
-# Create your tests here.
+
+class SubscribeTestCase(TestCase):
+    def setUp(self):
+        client = PaymeSubscribeCards(
+            base_url="https://checkout.test.paycom.uz/api/", 
+            paycom_id="5e730e8e0b852a417aa49ceb"
+        )
+        return client
+
+
+    def test_create_card(self):
+        client = self.setUp()
+        resp = client._cards_create(
+            number="8600495473316478",
+            expire="0399",
+            save=True,
+        )
+        self.assertEqual(resp['result']['card']['number'], '860049******6478')
+        self.assertEqual(resp['result']['card']['expire'], '03/99')
+        self.assertEqual(resp['result']['card']['recurrent'], True)
