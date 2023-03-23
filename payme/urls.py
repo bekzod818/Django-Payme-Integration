5629c33b427f69e4162c57090d30a3f2a331508a
bekzod818 <bekzod030900@gmail.com>
2023-03-23 22:10:33 +0500

    fix: FE

--- payme/urls.py
+++ payme/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from payme.views import MerchantAPIView
+from payme.views import MerchantAPIView, CreateCardView
 
 
 urlpatterns = [
+    path('', CreateCardView.as_view(), name="create_card"),
     path("merchant/", MerchantAPIView.as_view())
 ]
